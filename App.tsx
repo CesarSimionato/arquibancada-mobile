00f09a54e8c7458dd23cad41f8543cf0372bb26a
CesarSimionato <cesar.medeiros.simionato@gmail.com>
2024-05-06 23:24:48 -0300

    add navigation bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import "@/styles/global.css"
 
+import * as NavigationBar from 'expo-navigation-bar';
+
 import { StatusBar } from 'expo-status-bar';
+
 import { SignIn } from '@/app/SignIn';
+import { colors } from "@/styles/colors";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    const setup = async () => {
+      await NavigationBar.setBackgroundColorAsync(colors.primary[900]);
+    }
+    setup()
+  }, [])
+
   return (
     <>
       <SignIn />
